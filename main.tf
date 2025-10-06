e29e06379eaefbd0cfbd43e900637076f43e15e9
Michael Yocca <19916665+mjyocca@users.noreply.github.com>
2025-10-06 14:58:38 -0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.3.2"
+    }
+  }
+}
+
+variable "prefix" {
+  type = string
+}
+
+resource "random_pet" "this" {
+  prefix = var.prefix
+  length = 8
+}
+
+resource "random_pet" "cat" {
+  prefix = var.prefix
+  length = 1
+}
+
+resource "random_pet" "dog" {
+  prefix = var.prefix
+  length = 2
+}
+
+output "name" {
+  value = random_pet.this.id
+}
